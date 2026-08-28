27a38c02169480206f24e97c811252ab3da8772c
Emanuel Oly <olysancheznicolas@gmail.com>
2026-08-28 15:34:07 -0400

    feat: colores de fondo para ingreso y egreso

--- app/src/main/java/com/example/gastos/MainActivity.kt
+++ app/src/main/java/com/example/gastos/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -14,6 +16,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
@@ -27,6 +30,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -413,11 +417,24 @@ fun AplicacionGastos() {
             // LISTA DE MOVIMIENTOS
 
             movimientos.forEach { movimiento ->
-
+                val colorTipo = if (
+                    movimiento.tipo == TipoMovimiento.INGRESO
+                ) {
+                    Color(0xFF16A34A)
+                } else {
+                    Color(0xFFDC2626)
+                }
                 Row(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(vertical = 8.dp),
+                        .padding(vertical = 4.dp)
+                        .background(color = colorTipo, shape = RoundedCornerShape(8.dp))
+                        .border(
+                            width = 1.dp,
+                            color = Color(0xFFE2E8F0),
+                            shape = RoundedCornerShape(8.dp)
+                        )
+                        .padding(12.dp),
                     horizontalArrangement = Arrangement.SpaceBetween
                 ) {
 
